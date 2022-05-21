256843a8db59daf99d3b2056cdee08ca25f9dbab
Santiago Acuña <snt-26@hotmail.com>
2022-05-21 12:23:43 -0500

    Create DataAcces to connect SQL Server

---
+++ CRUD_MVC_5/CRUD_MVC_5/Data/DataAccess.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_MVC_5.Data
+{
+    public class DataAccess
+    {
+        private readonly string connectionStringSQL = ConfigurationManager.ConnectionStrings["cadena"].ToString();
+        private readonly string cadenaDaniel = ConfigurationManager.ConnectionStrings["cadenaDaniel"].ToString();
+        public string ConnectionStringSQL => connectionStringSQL;
+        public string CadenaDaniel => cadenaDaniel;
+        public DataAccess()
+        {
+        }
+    }
+}

bfbff4d22874bb03d11b303b5249f67d62bbf803
Santiago Acuña <snt-26@hotmail.com>
2022-05-21 12:55:55 -0500

    Add PersonaEntity

---
+++ CRUD_MVC_5/CRUD_MVC_5/Entities/PersonaEntity.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CRUD_MVC_5.Entities
+{
+    public class PersonaEntity
+    {
+        public int Id { get; set; }
+
+        [Display(Name ="Nombre")]
+        [MaxLength(50,ErrorMessage ="El campo {0} debe tener un máximo de {1} caratéres.")]
+        [Required(ErrorMessage ="El campo {0} es obligatorio.")]
+        public string Name { get; set; }
+
+        [Display(Name = "Apellido")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener un máximo de {1} caratéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string FirtsName { get; set; }
+
+        [Display(Name = "Correo")]
+        [EmailAddress(ErrorMessage ="El campo {0} no es valido.")]
+        [MaxLength(50, ErrorMessage = "El campo {0} debe tener un máximo de {1} caratéres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Email { get; set; }
+
+        [Display(Name = "Teléfono")]
+        [Phone(ErrorMessage ="El campo {0} debe ser numérico.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Phone { get; set; }
+    }
+}
